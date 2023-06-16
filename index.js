8bc7cb60036c2f9c8a0a9e0f999a508f5a6e6e18
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-16 17:25:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+module.exports = {
+  customerName: customerName,
+  upperCaseCustomerName: upperCaseCustomerName
+};
